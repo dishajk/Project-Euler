c8f9b82ff5b0c81682655c5733aa89fd3850c9e3
dishajk <djkuzhively@gmail.com>
2024-02-22 00:09:44 +0530

    working on power digit sum

---
+++ 016_power_digit_sum.cpp
@@ -0,0 +1,24 @@
+// https://projecteuler.net/problem=16
+// Tue  6 Dec 20:28:24 IST 2022
+// Disha Kuzhively
+
+#include <iostream>
+#include <stdio.h>
+#include <cmath>
+
+using namespace std;
+
+const int n = 15;
+int main()
+{
+	int sum = 0;
+	int modulo[15]; //to contain {number of cycle in 2mod10, 2mod100,}
+	for(int k = 1; k <= n; k++){
+		for(int i=1; i < 10; i++){
+			
+
+			pow(2,i)%10^k;
+		}
+	}
+	return 0;
+}
