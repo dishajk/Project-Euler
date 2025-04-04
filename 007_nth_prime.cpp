b02e812c4fcda660797d50fd0acbeb1f8804e29a
dishajk <djkuzhively@gmail.com>
2025-04-05 02:36:00 +0530

    implemented sieve of eratosthenes using vector

--- 007_nth_prime.cpp
+++ 007_nth_prime.cpp
@@ -5,36 +5,33 @@
 #include <iostream>
 #include <stdio.h>
 #include <cmath>
+#include <vector>
 
 using namespace std;
 
-const int n =10001;
-int prime[n]; 
-
-int divisibility(int, int);
 int main()
 {
-	prime[0] = 2;
-	int dummy;
-	for (int i = 0; i < n; i++)
-	{
-		dummy = prime[i];
-		do
-		{
-			dummy = dummy + 1;
-		} while (divisibility(dummy,i)==1);
-		prime[i+1]=dummy;
-	}
-	cout << prime[n-1] << "\n";
-	return 0;
-}
-int divisibility(int n, int k){
-	for (int i = 0; i < k+1; i++)
-	{
-		if (n%prime[i]==0) 
-		{
-			return 1; /*if divisible return 1*/
+const int n =10001;
+const int limit = n*log(n* log(n)); 
+
+vector<bool> isprime(limit,true);
+vector<int> primes;
+
+isprime[0] = false;
+isprime[1] = false;
+
+for (int i = 2; i < limit; i ++){
+	if(isprime[i]){
+		primes.push_back(i);
+		if(primes.size() == n){
+			cout << "The " << n <<"th prime number is "<<i<<endl;
+			break;
+		}
+		for (int j = 2*i; j < limit; j+=i){
+			isprime[j]=false;
 		}
 	}
+}
+cout << n <<"\t" << limit << endl;
 	return 0;
 }
--- readme.md
+++ readme.md
@@ -10,7 +10,8 @@
 
 |ID| Problem| Solution|
 |:---|:---|:---|
-|6|Sum Square Difference | [Solution - 006_SumSquareDifference.cpp](006_SumSquareDifference.cpp)|
+|7|[10001st Prime](https://projecteuler.net/problem=7)| [Solution - 007_nth_prime.cpp]|
+|6|[Sum Square Difference](https://projecteuler.net/problem=6) | [Solution - 006_SumSquareDifference.cpp](006_SumSquareDifference.cpp)|
 |5| Smallest Multiple | [Solution - 005_smallest_multiple.cpp](005_smallest_multiple.cpp)|
 |4| Largest Palindrome Product | [Solution - 004_Largestpalindromeproduct.cpp](004_Largestpalindromeproduct.cpp)|
 |3| Largest Prime Factor| [Solution - 003_primefactor.cpp](003_primefactor.cpp)|
